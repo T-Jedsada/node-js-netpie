bc7555982e13e88309d50df44736a40e52bc4494
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-07-02 13:16:13 +0700

    fix bug

--- src/netpie.js
+++ src/netpie.js
@@ -2,26 +2,31 @@ const MicroGear = require('microgear')
 
 const KEY = '5y8ffXzWQQj5ih1'
 const SECRET = '2aNnYbO9J0N8srOHsUC64KSIr'
+const ALIAS = '20scoopscnx-1234567890ABCD'
+const APPID = 'ihere'
 
 var microgear = MicroGear.create({
     key: KEY,
-    secret: SECRET
+    secret: SECRET,
+    alias: ALIAS
 })
 
 microgear.on('connected', function () {
+    microgear.subscribe("/door/status")
     console.log('Connected...')
-    microgear.setAlias("20scoopscnx-1234567890ABCD")
-    setInterval(function () {
-        microgear.chat('mygear', 'Hello world.')
-    }, 1000)
 })
 
-microgear.on('/door/status', function (topic, body) {
-    console.log('incoming : ' + topic + ' : ' + body)
-})
+microgear.on('message', function(topic,body) {
+    console.log('incoming topic: '+topic + '\tmessage: ' + body);
+    if (topic == '/ihere/door/status') {
+        // TODO : handler command
+    }
+});
 
 microgear.on('closed', function () {
     console.log('Closed...')
 })
+
+microgear.connect(APPID)
 
 module.exports = microgear
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const Hapi = require('hapi')
 const netpie = require('./netpie')
 
-const APPID = 'ihere'
 const server = Hapi.server({
     port: process.env.PORT || 8000
 })
 
-const doorCommandPostHandler = function (request, h) {
-    const commnad = request.payload.command
-    netpie.publish("/ihere/door", commnad);
+const doorCommandPostHandler = function (request, _) {
+    const command = request.payload.command
+    netpie.publish("/door", command, true)
     const reponse = {
         message: 'successfully'
     }
@@ -29,7 +28,6 @@ async function start() {
         process.exit(1)
     }
     console.log('Server running at:', server.info.uri)
-    netpie.connect(APPID)
 }
 
 start()
